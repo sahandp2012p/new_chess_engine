2d12e9d16493d79cdb96fa6023ea2a1c842cf78d
sahandp2012p <spourjavad2012@gmail.com>
2024-03-29 18:32:42 +0330

    Play until end of openings

---
+++ Openings.py
@@ -0,0 +1,20 @@
+#Opening db for python
+import chess
+import chess.polyglot
+import time
+
+
+board = chess.Board()
+
+with chess.polyglot.open_reader("./Books/books/varied.bin") as reader:
+
+   while True:
+    try:
+      moves = list(reader.find_all(board))
+      board.push(moves[0].move)
+      print(board)
+      time.sleep(1)
+    except:
+      print('No more moves')
+      break
+     
